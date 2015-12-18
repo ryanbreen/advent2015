9fad0d28d1904f0910220dd099634d89e4c0d3e5
Ryan Breen <rbreen@cimpress.com>
2015-12-17 22:55:36 -0500

    Finish day 3.

---
+++ 3/2/index.js
@@ -0,0 +1,38 @@
+var fs = require('fs');
+
+var bytes = fs.readFileSync(process.argv[2], 'utf8');
+
+var touched = { }
+
+var touch = function(x, y) {
+  touched[x + 'x' + y] = true;
+};
+
+[0, 1].forEach(function(start) {
+
+  var current_x = 0;
+  var current_y = 0;
+
+  touch(current_x, current_y);
+
+  for (var i=start; i<bytes.length; i+=2) {
+    switch(bytes[i]) {
+      case '^':
+        current_y += 1;
+        break;
+      case 'v':
+        current_y -= 1;
+        break;
+      case '>':
+        current_x += 1;
+        break;
+      case '<':
+        current_x -= 1;
+        break;
+    }
+
+    touch(current_x, current_y);
+  }
+});
+
+console.log(Object.keys(touched).length);

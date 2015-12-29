63067c80a1b768fec0b4bf55383635420ea52f96
Ryan Breen <rbreen@cimpress.com>
2015-12-29 09:29:34 -0500

    I swear this should be right.

--- 20/2/index.js
+++ 20/2/index.js
@@ -1,52 +1,33 @@
 var _ = require('underscore');
-var cluster = require('cluster');
 var target = parseInt(process.argv[2]);
 
 var process_house = function(num) {
   // Find all elves that would match this house
   var elves = [];
 
-  for (var i = (num/2)+1; i > 0; --i) {
-    if (num % i === 0) elves.push(i);
+  for (var i = (Math.floor(Math.sqrt(num))); i > 0; --i) {
+    if (num % i === 0) {
+      if (num <= i * 50) elves.push(i);
+      var complement = num/i;
+      if (complement != i && num <= complement * 50) elves.push(num/i);
+    }
   }
 
-  //console.log(num);
   //console.log(elves);
 
   return _.reduce(elves, function(memo, elf) {
     // If this is more than the 50th house for this elf, skip.
-    if (elf * 50 > num) return memo;
-
-    return memo + (elf * 10);
+    return memo + (elf * 11);
   }, 0);
 };
 
-var num = 5040000;
+var num = 0;
+var val = 0;
 
-var numCPUs = require('os').cpus().length;
-if (cluster.isMaster) {
-  // Fork workers.
-  for (var i = 0; i < numCPUs; i++) {
-    cluster.fork({NUM: num - i});
-  }
+while (val < target) {
 
-  cluster.on('exit', function(worker, code, signal) {
-    console.log('worker ' + worker.process.pid + ' died');
-  });
-} else {
-
-  /**/
-  var val = 0;
-  var num = process.env['NUM'];
-
-  while (val < target) {
-
-    val = process_house(num);
-
-    if (num % 10000 === 0) console.log("House %d received %d gifts, within %s of target", num, val, target - val);
-
-    num -= numCPUs;
-  }
-
-  console.log(num);
+  num += 1;
+  val = process_house(num);
 }
+
+console.log(num);

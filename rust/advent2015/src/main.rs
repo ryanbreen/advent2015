edc504de7d3af6fb22fb39dfafba9f7a52f3e573
Ryan Breen <rbreen@cimpress.com>
2016-01-17 23:26:50 -0500

    Day 1, part 2.

--- rust/advent2015/src/main.rs
+++ rust/advent2015/src/main.rs
@@ -4,5 +4,5 @@ mod calendar;
 fn main() {
     let calendar = calendar::Calendar::new();
     println!("There are {} days", calendar.days.len());
-    println!("{}", (calendar.days[0].part1.run)());
+    println!("{}", (calendar.days[0].part2.run)());
 }

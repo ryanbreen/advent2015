5b7a78e248f034a1a7a817a3a6ad8048e75da4a3
Ryan Breen <rbreen@cimpress.com>
2016-01-17 22:42:04 -0500

    Minor cleanup

--- rust/advent2015/src/calendar/day1.rs
+++ rust/advent2015/src/calendar/day1.rs
@@ -1,13 +1,13 @@
 
 fn part1 () -> String  {
-  "Day 1 part 1".to_string()
+  
 }
 
-pub const puzzle1 : super::Puzzle = super::Puzzle { 
-  run: part1,
-};
-
 pub const day1 : super::Day = super::Day {
-  part1: puzzle1,
-  part2: puzzle1
+  part1: super::Puzzle {
+    run: part1,
+  },
+  part2: super::Puzzle {
+    run: part1,
+  }
 };

09516679702734968809d6b04bb719776a0b5b63
Ryan Breen <rbreen@cimpress.com>
2016-01-19 00:01:51 -0500

    Switch to resource files and submodules for days.

---
+++ rust/advent2015/src/calendar/day1/mod.rs
@@ -0,0 +1,50 @@
+use std::str::CharRange;
+
+fn part1 (input: String) -> String  {
+  let mut i:usize = 0;
+  let mut total:i32 = 0;
+  while i < input.len() {
+      let CharRange {ch, next} = input.char_range_at(i);
+      if ch == '(' {
+        total += 1;
+      } else if ch == ')' {
+        total -= 1;
+      }
+      i = next;
+  }
+
+  return total.to_string();
+}
+
+fn part2 (input: String) -> String  {
+  let mut i:usize = 0;
+  let mut total:i32 = 0;
+  while i < input.len() {
+      let CharRange {ch, next} = input.char_range_at(i);
+      if ch == '(' {
+        total += 1;
+      } else if ch == ')' {
+        total -= 1;
+      }
+
+      i = next;
+
+      if total < 0 {
+        return i.to_string();
+      }
+  }
+
+  return i.to_string();
+}
+
+pub fn fill() -> super::Day {
+  return super::Day {
+    input: include_str!("input").to_string(),
+    part1: super::Puzzle {
+      run: part1,
+    },
+    part2: super::Puzzle {
+      run: part2,
+    }
+  };
+}

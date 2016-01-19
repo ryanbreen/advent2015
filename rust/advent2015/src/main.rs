ccd4b3fa32c5e897238b49555d9fd41b69884986
Ryan Breen <rbreen@cimpress.com>
2016-01-18 22:02:20 -0500

    Switch to getopts to handle CLI input.

--- rust/advent2015/src/main.rs
+++ rust/advent2015/src/main.rs
@@ -1,7 +1,39 @@
 #![feature(const_fn, str_char)]
 mod calendar;
 
+extern crate getopts;
+use getopts::Options;
+use std::env;
+
+fn print_usage(program: &str, opts: Options) {
+  let brief = format!("Usage: {} [options]", program);
+  print!("{}", opts.usage(&brief));
+}
+
 fn main() {
-    let calendar = calendar::Calendar::new();
+  let args: Vec<String> = env::args().collect();
+  let program = args[0].clone();
+
+  let mut opts = Options::new();
+  opts.reqopt("d", "", "The day to test (0-24).", "DAY");
+  opts.reqopt("p", "", "The puzzle to test (0-1).", "PUZZLE");
+  opts.optopt("i", "", "Alternate input text.", "INPUT");
+  opts.optflag("h", "help", "print this help menu");
+  let matches = match opts.parse(&args[1..]) {
+      Ok(m) => { m }
+      Err(f) => { panic!(f.to_string()) }
+  };
+  if matches.opt_present("h") {
+      print_usage(&program, opts);
+      return;
+  }
+  let day = matches.opt_str("d").expect("Missing value");
+  let first_puzzle = matches.opt_str("p").expect("Missing value") == "0";
+
+  let calendar = calendar::Calendar::new();
+  if first_puzzle {
+    println!("{}", (calendar.days[0].part1.run)(calendar.days[0].input));
+  } else {
     println!("{}", (calendar.days[0].part2.run)(calendar.days[0].input));
+  }
 }

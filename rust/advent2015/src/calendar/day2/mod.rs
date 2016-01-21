1961e75e3cc5822b8db49bad5b03278970de20a1
Ryan Breen <rbreen@cimpress.com>
2016-01-20 23:55:32 -0500

    Clean up debug.

--- rust/advent2015/src/calendar/day2/mod.rs
+++ rust/advent2015/src/calendar/day2/mod.rs
@@ -21,21 +21,45 @@ fn calculate_wrapping_paper_needs (input: String) -> u32 {
 }
 
 fn part1 (input: String) -> String  {
-
   let mut total = 0;
 
   let lines: Vec<&str> = input.lines().collect();
-  print!("{}\n", lines.len());
   for line in lines {
-    print!("{}\n", line);
     total += calculate_wrapping_paper_needs(line.to_string());
   }
 
   return total.to_string()
 }
 
+fn calculate_ribbon_needs (input: String) -> u32 {
+
+  let mut parts = input.split("x").collect::<Vec<&str>>();
+
+  let mut sides = Vec::new();
+  sides.push(parts[0].parse::<u32>().unwrap());
+  sides.push(parts[1].parse::<u32>().unwrap());
+  sides.push(parts[2].parse::<u32>().unwrap());
+
+  sides.sort();
+
+  let mut total = 0;
+  total += sides[0]*sides[1]*2;
+  total += sides[1]*sides[2]*2;
+  total += sides[0]*sides[2]*2;
+  total += sides[0]*sides[1];
+
+  return total;
+}
+
 fn part2 (input: String) -> String  {
-  return "noop".to_string()
+  let mut total = 0;
+
+  let lines: Vec<&str> = input.lines().collect();
+  for line in lines {
+    total += calculate_ribbon_needs(line.to_string());
+  }
+
+  return total.to_string()
 }
 
 pub fn fill() -> super::Day {

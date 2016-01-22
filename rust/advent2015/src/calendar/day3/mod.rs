7cda2e112efbda75dcdbbeac6275f0a43737d4be
Ryan Breen <rbreen@cimpress.com>
2016-01-22 00:27:18 -0500

    Finish day 3.

--- rust/advent2015/src/calendar/day3/mod.rs
+++ rust/advent2015/src/calendar/day3/mod.rs
@@ -30,7 +30,50 @@ fn part1 (input: String) -> String  {
 }
 
 fn part2 (input: String) -> String  {
-  return "noop".to_string();
+  let mut x = 0;
+  let mut y = 0;
+
+  let mut i:usize = 0;
+
+  let mut visited_houses = HashSet::new();
+  visited_houses.insert(format!("{},{}", x, y));
+
+  while i < input.len() {
+    let CharRange {ch, next} = input.char_range_at(i);
+    if ch == '^' {
+      y += 1;
+    } else if ch == 'v' {
+      y -= 1;
+    } else if ch == '>' {
+      x += 1;
+    } else if ch == '<' {
+      x -= 1;
+    }
+    i = next+1;
+
+    visited_houses.insert(format!("{},{}", x, y));
+  }
+
+  x = 0;
+  y = 0;
+  i = 1;
+  while i < input.len() {
+    let CharRange {ch, next} = input.char_range_at(i);
+    if ch == '^' {
+      y += 1;
+    } else if ch == 'v' {
+      y -= 1;
+    } else if ch == '>' {
+      x += 1;
+    } else if ch == '<' {
+      x -= 1;
+    }
+    i = next+1;
+
+    visited_houses.insert(format!("{},{}", x, y));
+  }
+
+  return visited_houses.len().to_string();
 }
 
 pub fn fill() -> super::Day {

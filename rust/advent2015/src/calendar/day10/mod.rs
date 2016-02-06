bb355f54a598d5d12c96f05dcc0d580d2b695211
Ryan Breen <rbreen@cimpress.com>
2016-02-06 12:43:27 -0500

    Day 10, part 1.

--- rust/advent2015/src/calendar/day10/mod.rs
+++ rust/advent2015/src/calendar/day10/mod.rs
@@ -1,6 +1,40 @@
+use std::str::CharRange;
+
+fn permute(input: String) -> String {
+
+  let mut i:usize = 1;
+  let CharRange {ch, ..} = input.char_range_at(0);
+  let mut last_char = ch;
+  let mut last_char_count:usize = 1;
+  let mut rvalue:String = String::new();
+
+  while i < input.len() {
+    let CharRange {ch, next} = input.char_range_at(i);
+    if ch != last_char {
+      // We need to add the count of the last char to the return string.
+      rvalue.push_str(&last_char_count.to_string());
+      rvalue.push(last_char);
+      last_char_count = 1;
+      last_char = ch;
+    } else {
+      last_char_count += 1;
+    }
+    i = next;
+  }
+
+  rvalue.push_str(&last_char_count.to_string());
+  rvalue.push(last_char);
+
+  return rvalue;
+}
 
 fn part1(input: String) -> String  {
-  return input;
+  let mut permuted = input;
+  for _ in 0..40 {
+    permuted = permute(permuted);
+  }
+
+  return permuted.len().to_string();
 }
 
 fn part2 (input: String) -> String  {

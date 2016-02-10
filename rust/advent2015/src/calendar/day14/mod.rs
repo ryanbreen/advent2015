e27056088944dfc2ac27aebd00944e4371552957
Ryan Breen <rbreen@cimpress.com>
2016-02-10 00:00:46 -0500

    Day 14 scaffolding.

---
+++ rust/advent2015/src/calendar/day14/mod.rs
@@ -0,0 +1,43 @@
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
+
+fn part1 (input: String) -> String {
+  let mut buffer = String::new();
+  let mut f = File::open(Path::new(&input)).unwrap();
+  let _ = f.read_to_string(&mut buffer);
+
+  return buffer;
+}
+
+fn part2 (input: String) -> String {
+  let mut buffer = String::new();
+  let mut f = File::open(Path::new(&input)).unwrap();
+  let _ = f.read_to_string(&mut buffer);
+
+  return buffer;
+}
+
+pub fn fill() -> super::Day {
+  return super::Day {
+    input: "./src/calendar/day14/input".to_string(),
+    part1: super::Puzzle {
+      run: part1,
+    },
+    part2: super::Puzzle {
+      run: part2,
+    }
+  };
+}
+
+#[test]
+fn test_part1() {
+  let day = fill();
+  assert_eq!((day.part1.run)(day.input.to_string()), "709".to_string());
+}
+
+#[test]
+fn test_part2() {
+  let day = fill();
+  assert_eq!((day.part2.run)(day.input.to_string()), "668".to_string());
+}

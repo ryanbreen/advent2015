ec816926efab87c42b45a56cc270ddb37829c9f8
Ryan Breen <rbreen@cimpress.com>
2016-02-09 23:44:43 -0500

    Day 13, part 2.

--- rust/advent2015/src/calendar/day13/mod.rs
+++ rust/advent2015/src/calendar/day13/mod.rs
@@ -110,7 +110,74 @@ fn part1(input: String) -> String  {
 }
 
 fn part2 (input: String) -> String {
-  return input;
+  let mut buffer = String::new();
+  let mut f = File::open(Path::new(&input)).unwrap();
+  let _ = f.read_to_string(&mut buffer);
+
+  let mut people:HashMap<String, Person> = HashMap::new();
+
+  let lines: Vec<&str> = buffer.lines().collect();
+  for line in lines {
+    let parts: Vec<&str> = line.split(' ').collect();
+
+    let partner = parts[10].split('.').next().unwrap();
+
+    let connection_parts = [ (parts[0], partner), (partner, parts[0]) ];
+    for connection_part in &connection_parts {
+      let principal = match people.entry(connection_part.0.to_string()) {
+        Entry::Occupied(o) => o.into_mut(),
+        Entry::Vacant(v) => v.insert(Person { connections: Box::new(BTreeSet::new()) })
+      };
+
+      let ref mut connections:BTreeSet<Connection> = *(principal.connections.borrow_mut());
+      let mut connection_weight = 0;
+
+      let stub_connection = Connection { partner: "fake".to_string(), net_weight: 0 };
+      let mut matched_connection:Connection = stub_connection.clone();
+
+      for connection in connections.iter().cloned() {
+        if connection.partner == connection_part.1 {
+          connection_weight = connection.net_weight;
+          matched_connection = connection.clone();
+        }
+      }
+
+      if matched_connection != stub_connection {
+        connections.remove(&matched_connection);
+      }
+
+      if parts[2] == "gain" {
+        connection_weight += parts[3].parse::<i16>().unwrap();
+      } else {
+        connection_weight -= parts[3].parse::<i16>().unwrap();
+      }
+      connections.insert(Connection {net_weight: 0, partner: "me".to_string()});
+      connections.insert(Connection { net_weight: connection_weight, partner: connection_part.1.to_string() });
+    }
+  }
+
+  let mut me = Person { connections: Box::new(BTreeSet::new()) };
+
+  {
+    let ref mut my_connections:BTreeSet<Connection> = *(me.connections.borrow_mut());
+    for (name, _) in &people {
+      my_connections.insert(Connection {net_weight: 0, partner: name.to_string()});
+    }
+  }
+
+  people.insert("me".to_string(), me);
+
+  // At this point, we have all of our people and connections in a heap.  Let's iterate from person to person to
+  // find the most utilitarian arrangement.
+  let mut highest:i16 = 0;
+  for (person, _) in &people {
+    let current = test_connections(&person, &people);
+    if current > highest {
+      highest = current;
+    }
+  }
+
+  return highest.to_string();
 }
 
 pub fn fill() -> super::Day {

198d5787ebedbcb7c8ef810f5e2be25d44a0cf42
Ryan Breen <rbreen@cimpress.com>
2016-03-28 21:18:07 -0400

    Day 20, part 2.

--- rust/advent2015/src/calendar/day20/mod.rs
+++ rust/advent2015/src/calendar/day20/mod.rs
@@ -16,6 +16,26 @@ fn count_for_house(num: u32) -> u32 {
   return counter;
 }
 
+fn adjusted_count_for_house(num: u32) -> u32 {
+  let limit = ((num as f64).sqrt() as u32) + 1;
+
+  let mut counter:u32 = 0;
+  for i in 1..limit {
+    if num % i == 0 {
+      // Add a visit for the number and its complement (unless it's a square);
+      if num <= 50 * i {
+        counter += 11*i;
+      }
+
+      if num / i != i && num <= 50 * (num / i) {
+        counter += 11 * (num / i);
+      }
+    }
+  }
+
+  return counter;
+}
+
 #[allow(dead_code)]
 fn sanity_check() {
   for i in 1..10 {
@@ -40,9 +60,21 @@ fn part1 (input: String) -> String {
 }
 
 fn part2 (input: String) -> String {
-  let count = 0;
+  let target = input.parse::<u32>().unwrap();
 
-  return count.to_string();
+  let mut i = 1;
+  loop {
+    let count = count_for_house(i);
+
+    if count >= target {
+      let count = adjusted_count_for_house(i);
+      if count >= target {
+        return i.to_string();
+      }
+    }
+
+    i += 1;
+  }
 }
 
 pub fn fill() -> super::Day {
@@ -66,5 +98,5 @@ fn test_part1() {
 #[test]
 fn test_part2() {
   let day = fill();
-  assert_eq!((day.part2.run)(day.input.to_string()), "200".to_string());
+  assert_eq!((day.part2.run)(day.input.to_string()), "884520".to_string());
 }
